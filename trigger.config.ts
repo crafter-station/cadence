da7dc3e513764b7e157f6d0de50e84b55376f306
Cris <cristian.correa.cs@gmail.com>
2026-01-31 18:41:09 -0500

    Hardcode trigger project ID for Docker build

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
-import { config } from "dotenv";
-
-config();
-
-if (!process.env.TRIGGER_PROJECT_ID) {
-  throw new Error("TRIGGER_PROJECT_ID is not set");
-}
 
 export default defineConfig({
-  project: process.env.TRIGGER_PROJECT_ID,
+  project: "proj_yyctxlrmczifzeimrskm",
   runtime: "node",
   logLevel: "log",
   maxDuration: 3600,
