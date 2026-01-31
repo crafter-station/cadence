528b4863324e31eff0f4b34511e03eb1c72b010b
Cris <cristian.correa.cs@gmail.com>
2026-01-31 14:15:30 -0500

    Redirect to dashboard after sign in/up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider appearance={{ baseTheme: dark }}>
+    <ClerkProvider
+      appearance={{ baseTheme: dark }}
+      signInFallbackRedirectUrl="/app"
+      signUpFallbackRedirectUrl="/app"
+    >
       <html lang="en">
         <body className={`font-sans antialiased`}>
           <Providers>
